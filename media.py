892243148fd771bca7fc0b6036b62f8acdc83737
Jay Sheth <shethju@gmail.com>
2017-11-10 23:33:22 -0800

    Add files via upload

--- media.py
+++ media.py
@@ -1,7 +1,13 @@
+import webbrowser
+
 class Movie():
     def __init__(self, title, story, img, yt):
         self.title = title
         self.story = story
         self.poster_image_url = img
         self.trailer_youtube_url = yt
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+    
         
